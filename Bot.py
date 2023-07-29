c8f223e5035810b08046a08fbef5de64eaf77347
IamBuildDifferent <137944124+IamBuildDifferent@users.noreply.github.com>
2023-07-29 11:14:23 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -4,30 +4,6 @@ import datetime
 import shutil
 import requests
 
-Form = '''title: Bilder des Monats Mai 2023
-+ description:
-+ Bilder des Monats Mai aus unserer Community im Discord.
-+ type: activity
-+ image: /aktivitaeten/BDM-2023-05/bilderdesmonats_2023-04_2.jpg
-+ features:
-+ - FOTOS
-+ --- +
-+ # Bilder des Monats: Mai 2023 +
-+ Ohne viele Worte einige Bilder der Draußenfunker. +
-+ DM3KP – Achim:
-+ ![Bilder des Monats](/aktivitaeten/BDM-2023-05/bilderdesmonats_2023-05_01.jpg) +
-+ ![Bilder des Monats](/aktivitaeten/BDM-2023-05/bilderdesmonats_2023-05_02.jpg) +
-+ ![Bilder des Monats](/aktivitaeten/BDM-2023-05/bilderdesmonats_2023-05_03.jpg) +
-+ ![Bilder des Monats](/aktivitaeten/BDM-2023-05/bilderdesmonats_2023-05_04.jpg) +
-+ ![Bilder des Monats](/aktivitaeten/BDM-2023-05/bilderdesmonats_2023-05_05.jpg) +
-+ DK4HAA – Harm:
-+ ![Bilder des Monats](/aktivitaeten/BDM-2023-05/bilderdesmonats_2023-05_06.jpg) +
-+ ![Bilder des Monats](/aktivitaeten/BDM-2023-05/bilderdesmonats_2023-05_07.jpg) +
-+ DF9HC – Haiko: DA-0244. 20m SSB mit FT-891, Groundplane. +
-+ ![Bilder des Monats](/aktivitaeten/BDM-2023-05/bilderdesmonats_2023-05_08.jpg) +
-+ ![Bilder des Monats](/aktivitaeten/BDM-2023-05/bilderdesmonats_2023-05_09.jpg) +
-+ ![Bilder des Monats](/aktivitaeten/BDM-2023-05/bilderdesmonats_2023-05_10.jpg) +
-'''
 DescriptionImmageList = []
 
 Count = 1
@@ -35,6 +11,24 @@ YearMonth = input("Month.Year :")
 YearMonth = YearMonth.split('.')
 Month = YearMonth[0]
 Year = YearMonth[1]
+
+TopForm = '''
+title: Bilder des Monats {MonthName} {YearNumber}
++ description:
++ Bilder des Monats {MonthName} aus unserer Community im Discord.
++ type: activity
++ image: {CoverImg}
++ features:
++ - FOTOS
++ --- +
++ # Bilder des Monats: {MonthName} {YearNumber} +
++ Ohne viele Worte einige Bilder der Draußenfunker. +'''.format(YearNumber=Year)#
+
+MiddleForm
+'''
++ {Description}
++ ![Bilder des Monats]({ImgPath}) +
+'''
 
 client = discord.Client(intents=discord.Intents.all()) #intents=discord.Intents.all() is only sometimes necesary, if you get an error remove it!
 
