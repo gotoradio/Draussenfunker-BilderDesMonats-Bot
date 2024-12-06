f3358df88a23ea2f9c3c23ce5e518573d3e62fa9
gotoradio <137944124+gotoradio@users.noreply.github.com>
2024-12-06 17:54:57 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,5 +1,5 @@
-from discord.ext import commands
-import discord, datetime, shutil, requests, random
+import discord, datetime, shutil, requests, random, asyncio, subprocess
+import secrets
 
 MonthName = {'01': 'Januar', '02': 'Februar', '03': 'März', '04': 'April', '05': 'Mai', '06': 'Juni', '07': 'Juli', '08': 'August', '09': 'September', '10': 'Oktober', '11': 'November', '12': 'Dezember'}
 vuepressImmages = ''
@@ -11,11 +11,15 @@ YearMonth = YearMonth.split('.')
 Month = YearMonth[0]
 Year = YearMonth[1]
 
-print(f'Run: cd draussenfunker.github.io; git pull; mkdir docs/.vuepress/public/aktivitaeten/BDM-{Year}-{Month}/')
-input("Ready?")
+print('Pulling the repository and creating a picture dir...')
+if subprocess.call(['git', 'pull'], cwd='draussenfunker.github.io') != 0:
+    exit('Error, can not pull the repository, Exiting')
+    pass
+if subprocess.call(['mkdir', f'docs/.vuepress/public/aktivitaeten/BDM-{Year}-{Month}/'], cwd='draussenfunker.github.io') != 0:
+    #exit('Error, can not create picture dir, Exiting')
+    pass
+print('Ready')
 
-# intents=discord.Intents.all() is only sometimes necesary, if you get an error remove it!
-# ^^^ Later me has no clue what earlier me ment.
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
@@ -26,8 +30,8 @@ async def on_ready():
     global vuepressImmages
     global error
 
-    channel = client.get_channel(****)
-    async for message in channel.history(limit=500, after=datetime.datetime(int(Year), int(Month), 1)):
+    channel = client.get_channel(secrets.channelID)
+    async for message in channel.history(limit=500, after=datetime.datetime(int(Year), int(Month), 1),): #before=datetime.datetime(int(Year), int(Month) + 1, 1)): <-- dos not work for December 12 + 1 = 13
         if message.attachments != []:
             if message.content == '':
                 print('\n\nNo description')
@@ -39,11 +43,12 @@ async def on_ready():
             else:
                 print(f'\n\nDescription: {message.content}')
 
+                # <@614242831434776576> <#1062798608581267636>
                 if '<@' in message.content or '<#' in message.content:
                     print('Error: @ or # in message')
 
                     line = vuepressImmages.count('\n') + 2
-                    error += f'@ or # in line {line}\n'
+                    error += f'@ or # in line {line}\n'                     # Line count dos not work properly
 
                 vuepressImmages += f'\n{message.content}\n\n'
 
@@ -55,7 +60,7 @@ async def on_ready():
                 ImmageName = f'{ImmageCount:02d}_{Month}-{Year}.jpg'
                 LocalPath = f'draussenfunker.github.io/docs/.vuepress/public/aktivitaeten/BDM-{Year}-{Month}/{ImmageName}'
 
-                shutil.copyfileobj(requests.get(Attatchment.url, stream=True).raw, open(LocalPath, 'wb'))
+                shutil.copyfileobj(requests.get(Attatchment.url, stream=True).raw, open(LocalPath, 'wb'))                       # needs to be async and also save .png not as .jpg
 
                 vuepressImmages += f'![Bilder des Monats](/aktivitaeten/BDM-{Year}-{Month}/{ImmageName})\n'
                 ImmageCount += 1
@@ -63,7 +68,7 @@ async def on_ready():
     print('\n\nClosing Discord')
     await client.close()
 
-client.run('****')
+client.run(secrets.botToken)
 
 print('\nNumber of tite Immage the following format: 01')
 CoverImgNr = input('Title immage: ')
@@ -92,4 +97,11 @@ if error != '':
     print(error)
     input('Hit return to continue')
 
-print(f'Run: cd draussenfunker.github.io; git add -A; git commit -m \"Bilder des Monats {MonthName[Month]}\"; git push')
+input('Crate commit and push it to the remote repository')
+if subprocess.call(['git', 'add', '-A'], cwd='draussenfunker.github.io') != 0:
+    Exit('Error, can not add Files to commit')
+if subprocess.call(['git', 'commit', '-m', f'\"Bilder des Monats {MonthName[Month]}\"'], cwd='draussenfunker.github.io') != 0:
+    Exit('Error, can not commit')
+if subprocess.call(['git', 'push'], cwd='draussenfunker.github.io') != 0:
+    Exit('Error, can not push the remote Repository')
+print('All done, Exiting')
